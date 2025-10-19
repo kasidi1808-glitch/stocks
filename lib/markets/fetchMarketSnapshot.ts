c7c48822445f948384aa13efefe6b214cb790980
kasidi1808-glitch <kasidi1808@gmail.com>
2025-10-19 20:01:56 +0700

    Wrap placeholder quotes with instrument overrides

--- lib/markets/fetchMarketSnapshot.ts
+++ lib/markets/fetchMarketSnapshot.ts
@@ -3,38 +3,48 @@ import { unstable_noStore as noStore } from "next/cache"
 import type { Quote } from "@/types/yahoo-finance"
 
 import { loadQuotesForSymbols } from "../yahoo-finance/fetchQuote"
+import { getOfflineQuote } from "@/data/offlineQuotes"
 
 import type { MarketInstrument } from "./types"
 
 function createPlaceholderQuote(instrument: MarketInstrument): Quote {
-  return {
-    symbol: instrument.symbol,
-    shortName: instrument.shortName,
-    regularMarketPrice: null,
-    regularMarketChange: null,
-    regularMarketChangePercent: null,
-    regularMarketDayLow: null,
-    regularMarketDayHigh: null,
-    fiftyTwoWeekLow: null,
-    fiftyTwoWeekHigh: null,
-    marketCap: null,
-    regularMarketVolume: null,
-    averageDailyVolume3Month: null,
-    regularMarketOpen: null,
-    regularMarketPreviousClose: null,
-    trailingEps: null,
-    trailingPE: null,
-    fullExchangeName: null,
-    currency: null,
-    regularMarketTime: null,
-    postMarketPrice: null,
-    postMarketChange: null,
-    postMarketChangePercent: null,
-    preMarketPrice: null,
-    preMarketChange: null,
-    preMarketChangePercent: null,
-    hasPrePostMarketData: false,
+  const offlineQuote = getOfflineQuote(instrument.symbol)
+
+  if (offlineQuote) {
+    return applyInstrumentOverrides(offlineQuote, instrument)
   }
+
+  return applyInstrumentOverrides(
+    {
+      symbol: instrument.symbol,
+      shortName: instrument.shortName,
+      regularMarketPrice: null,
+      regularMarketChange: null,
+      regularMarketChangePercent: null,
+      regularMarketDayLow: null,
+      regularMarketDayHigh: null,
+      fiftyTwoWeekLow: null,
+      fiftyTwoWeekHigh: null,
+      marketCap: null,
+      regularMarketVolume: null,
+      averageDailyVolume3Month: null,
+      regularMarketOpen: null,
+      regularMarketPreviousClose: null,
+      trailingEps: null,
+      trailingPE: null,
+      fullExchangeName: null,
+      currency: null,
+      regularMarketTime: null,
+      postMarketPrice: null,
+      postMarketChange: null,
+      postMarketChangePercent: null,
+      preMarketPrice: null,
+      preMarketChange: null,
+      preMarketChangePercent: null,
+      hasPrePostMarketData: false,
+    },
+    instrument
+  )
 }
 
 function applyInstrumentOverrides(
